72a93c6e215a4943334096a2ee5e5e9a1a3f61f7
aleksmat207 <aleksandra.matlingiewicz@gmail.com>
2018-11-21 00:20:03 +0100

    checking cards, changing rule, counters

--- wcstApp/wcstApp/wwwroot/angular-wcst/src/app/cards/cards.component.ts
+++ wcstApp/wcstApp/wwwroot/angular-wcst/src/app/cards/cards.component.ts
@@ -1,102 +1,140 @@
-import { Component, OnInit } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '../../../node_modules/@angular/platform-browser';
-import { CardsModel } from '../service/CardsService/cards.model';
-import { CardsService } from '../service/CardsService/cards.service';
-import { RuleService } from '../service/RuleService/rule.service';
-import { RuleModel } from '../service/RuleService/rule.model';
-import { AngularWaitBarrier } from '../../../node_modules/blocking-proxy/built/lib/angular_wait_barrier';
-import { equal } from 'assert';
+import { Component, OnInit } from "@angular/core";
+import {
+  DomSanitizer,
+  SafeResourceUrl
+} from "../../../node_modules/@angular/platform-browser";
+import { CardsModel } from "../service/CardsService/cards.model";
+import { CardsService } from "../service/CardsService/cards.service";
+import { RuleService } from "../service/RuleService/rule.service";
+import { RuleModel } from "../service/RuleService/rule.model";
 
 @Component({
-  selector: 'app-cards',
-  templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+  selector: "app-cards",
+  templateUrl: "./cards.component.html",
+  styleUrls: ["./cards.component.css"]
 })
 export class CardsComponent implements OnInit {
+  startingCards: Array<CardsModel> = [];
+  card: CardsModel;
+  previousCard: CardsModel;
+  imagePaths: Array<SafeResourceUrl> = [];
+  randomImagePath: SafeResourceUrl;
+  isRuleChecked: boolean;
+  decodedCards: Array<string>;
+  rule: RuleModel;
+  ruleCounter: number;
+  mistakeCounter: number;
+  result:string;
+  isStarted:boolean;
+  previousRule: RuleModel = { name };
 
-startingCards: Array<CardsModel>=[];
-card:CardsModel;
-previousCard:CardsModel;
-imagePaths:Array<SafeResourceUrl>=[];
-randomImagePath:SafeResourceUrl;
-decodedCards: Array<string>;
-rule:RuleModel;
-previousRule:RuleModel={name};
-
-  constructor(private cardsService: CardsService, private sanitizer : DomSanitizer, private ruleService: RuleService,) { }
+  constructor(
+    private cardsService: CardsService,
+    private ruleService: RuleService
+  ) {}
 
   ngOnInit() {
+    this.mistakeCounter = 0;
+    this.ruleCounter = 0;
+    this.getRandomRule();
   }
-getStartingCards(){
-  this.cardsService.getStartingCards().subscribe(
-    r=>{
-      this.startingCards=r;
-     // console.log(this.startingCards) 
-      this.decode()
-    }
-  )
-}
-decode(){
-    this.imagePaths=[]
-    this.startingCards.forEach(card => {
-  this.imagePaths.push(this.sanitizer.bypassSecurityTrustUrl('data:image/png;base64,' + card.imgbase));
-  //console.log(this.imagePaths)
-});
-}
-getRandomCard(){
-    this.cardsService.getRandomCard().subscribe(
-        r=>{
-            this.card=r;
-            this.isCardRepeated();
-            this.randomImagePath=this.sanitizer.bypassSecurityTrustUrl('data:image/png;base64,' + r.imgbase);
+  getStartingCards() {
+    this.cardsService.getStartingCards().subscribe(r => {
+      this.startingCards = r;
+      this.isStarted=true;
+      // console.log(this.startingCards)
+      //  this.decode()
+    });
+  }
+  // decode(){
+  //     this.imagePaths=[]
+  //     this.startingCards.forEach(card => {
+  //   this.imagePaths.push(this.sanitizer.bypassSecurityTrustUrl('data:image/png;base64,' + card.imgbase));
+  // });
+  // }
+  getRandomCard() {
+    this.cardsService.getRandomCard().subscribe(r => {
+      this.card = r;
+      this.isCardRepeated();
+      //this.randomImagePath=this.sanitizer.bypassSecurityTrustUrl('data:image/png;base64,' + r.imgbase);
+    });
+  }
+  checkCard(element) {
+    console.log(element);
+
+    switch (this.rule.name) {
+      case "color":
+        if (element.color == this.card.color) {
+          this.ruleCounter = this.ruleCounter + 1;
+          console.log("DOBRZE! ruleCounter:", this.ruleCounter);
+          this.result="Dobrze!";
+        } else {
+          this.mistakeCounter++;
+          console.log("ZLE! mistakeCounter:", this.mistakeCounter);
+          this.result="Źle!";
         }
-    )
-}
-checkCard(){
-    switch(this.rule.name){
-        case "color":
-        
         break;
-        case "sign":
+      case "sign":
+      if (element.sign == this.card.sign) {
+        this.ruleCounter = this.ruleCounter + 1;
+        console.log("DOBRZE! ruleCounter:", this.ruleCounter);
+        this.result="Dobrze!";
+      } else {
+        this.mistakeCounter++;
+        console.log("ZLE! mistakeCounter:", this.mistakeCounter);
+        this.result="Źle!";
+      }
         break;
-        case "case":
+      case "amount":
+      if (element.amount == this.card.amount) {
+        this.ruleCounter = this.ruleCounter + 1;
+        console.log("DOBRZE! ruleCounter:", this.ruleCounter);
+        this.result="Dobrze!";
+      } else {
+        this.mistakeCounter++;
+        console.log("ZLE! mistakeCounter:", this.mistakeCounter);
+        this.result="Źle!";
+      }
         break;
     }
-}
-isCardRepeated(){
-    if(JSON.stringify(this.card) === JSON.stringify(this.previousCard)){
-        console.log("KARTY SIE POWTORZYLY!!!!")
+    this.getRandomCard();
+    if(this.ruleCounter==10){
+        this.ruleCounter=0;
+        console.log("ZMIANA REGUŁY! ruleCounter:", this.ruleCounter);
+        this.getRandomRule();
+    }
+  }
+  isCardRepeated() {
+    if (JSON.stringify(this.card) === JSON.stringify(this.previousCard)) {
+      console.log("KARTY SIE POWTORZYLY!!!!");
 
-   this.getRandomCard() }
-   else{  console.log(2)
-       this.previousCard=this.card;
-   }
-}
-getRandomRule(){
-    this.ruleService.getRandomRule().subscribe(
-        r=>{
-            this.rule=r;
-            this.isRuleRepeated();
-        }
-    )
-   console.log(this.rule)
-
-}
-isRuleRepeated(){
-    if(JSON.stringify(this.rule).toLowerCase() === JSON.stringify(this.previousRule).toLowerCase()){
-        console.log(1)
-
-   this.getRandomRule() }
-   else{  console.log(2)
-       this.previousRule=this.rule;
-   }
-}
-// dataURItoBlob(binary) {
-//   var array = [];
-//    array.push(binary);
-// return new Blob([new Uint8Array(array)], {
-//   type: 'image/png'
-// });
-// }
-
+      this.getRandomCard();
+    } else {
+      this.previousCard = this.card;
+    }
+  }
+  getRandomRule() {
+    this.ruleService.getRandomRule().subscribe(r => {
+      this.rule = r;
+      this.isRuleRepeated();
+      this.isRuleChecked = true;
+    });
+  }
+  isRuleRepeated() {
+    if (
+      JSON.stringify(this.rule).toLowerCase() ===
+      JSON.stringify(this.previousRule).toLowerCase()
+    ) {
+      this.getRandomRule();
+    } else {
+      this.previousRule = this.rule;
+    }
+  }
+  // dataURItoBlob(binary) {
+  //   var array = [];
+  //    array.push(binary);
+  // return new Blob([new Uint8Array(array)], {
+  //   type: 'image/png'
+  // });
+  // }
 }
