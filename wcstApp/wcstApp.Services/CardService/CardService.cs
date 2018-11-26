5434b3a63d7026da3ae608b539e8f116df8557d9
aleksmat207 <aleksandra.matlingiewicz@gmail.com>
2018-11-26 23:52:32 +0100

    scoring, errors

--- wcstApp/wcstApp.Services/CardService/CardService.cs
+++ wcstApp/wcstApp.Services/CardService/CardService.cs
@@ -369,6 +369,302 @@ namespace wcstApp.Services.CardService
                    Color ="yellow",
                Number=4,
                Form="star"
+                },
+                 new CardModel() {Imgbase="assets/cards/1circleBlue.png",
+                   Color ="blue",
+               Number=1,
+               Form="circle"},
+                new CardModel() {Imgbase="assets/cards/1circleGreen.png",
+                   Color ="green",
+               Number=1,
+               Form="circle"
+                },
+                new CardModel() {Imgbase="assets/cards/1circleYellow.png",
+                   Color ="yellow",
+               Number=1,
+               Form="circle"
+                },
+                new CardModel() {Imgbase="assets/cards/1circleRed.png",
+                   Color ="red",
+               Number=1,
+               Form="circle"
+                },
+                new CardModel() {Imgbase="assets/cards/1crossBlue.png",
+                   Color ="blue",
+               Number=1,
+               Form="cross"
+                },
+                 new CardModel() {Imgbase="assets/cards/1crossGreen.png",
+                   Color ="green",
+               Number=1,
+               Form="cross"
+                },
+                new CardModel() {Imgbase="assets/cards/1crossRed.png",
+                   Color ="red",
+               Number=1,
+               Form="cross"
+                },
+                new CardModel() {Imgbase="assets/cards/1crossYellow.png",
+                   Color ="yellow",
+               Number=1,
+               Form="cross"
+                },
+                new CardModel() {Imgbase="assets/cards/1squareBlue.png",
+                   Color ="blue",
+               Number=1,
+               Form="square"
+                },
+                new CardModel() {Imgbase="assets/cards/1squareGreen.png",
+                   Color ="green",
+               Number=1,
+               Form="square"
+                },
+                new CardModel() {Imgbase="assets/cards/1squareYellow.png",
+                   Color ="yellow",
+               Number=1,
+               Form="square"
+               },
+                new CardModel() {Imgbase="assets/cards/1starBlue.png",
+                   Color ="blue",
+               Number=1,
+               Form="star"},
+                new CardModel() {Imgbase="assets/cards/1starGreen.png",
+                   Color ="green",
+               Number=1,
+               Form="star"
+                },
+                new CardModel() {Imgbase="assets/cards/1starRed.png",
+                   Color ="red",
+               Number=1,
+               Form="star"
+                },
+                new CardModel() {Imgbase="assets/cards/1starYellow.png",
+                   Color ="yellow",
+               Number=1,
+               Form="star"
+                },
+                new CardModel() {Imgbase="assets/cards/2circleBlue.png",
+                   Color ="blue",
+               Number=2,
+               Form="circle"
+                },
+                new CardModel() {Imgbase="assets/cards/2circleGreen.png",
+                   Color ="green",
+               Number=2,
+               Form="circle"
+                },
+                 new CardModel() {Imgbase="assets/cards/2circleRed.png",
+                   Color ="red",
+               Number=2,
+               Form="circle"
+                },
+                new CardModel() {Imgbase="assets/cards/2circleYellow.png",
+                   Color ="yellow",
+               Number=2,
+               Form="circle"
+                },
+                new CardModel() {Imgbase="assets/cards/2crossBlue.png",
+                   Color ="blue",
+               Number=2,
+               Form="cross"
+                },
+                new CardModel() {Imgbase="assets/cards/2crossGreen.png",
+                   Color ="green",
+               Number=2,
+               Form="cross"
+                },
+                new CardModel() {Imgbase="assets/cards/2crossRed.png",
+                   Color ="red",
+               Number=2,
+               Form="cross"
+                },
+                new CardModel() {Imgbase="assets/cards/2crossYellow.png",
+                   Color ="yellow",
+               Number=2,
+               Form="cross"
+               },
+
+                new CardModel() {Imgbase="assets/cards/2squareBlue.png",
+                   Color ="blue",
+               Number=2,
+               Form="square"},
+                new CardModel() {Imgbase="assets/cards/2squareGreen.png",
+                   Color ="green",
+               Number=2,
+               Form="square"
+                },
+                new CardModel() {Imgbase="assets/cards/2squareRed.png",
+                   Color ="red",
+               Number=2,
+               Form="square"
+                },
+                new CardModel() {Imgbase="assets/cards/2squareYellow.png",
+                   Color ="yellow",
+               Number=2,
+               Form="square"
+                },
+                new CardModel() {Imgbase="assets/cards/2starBlue.png",
+                   Color ="blue",
+               Number=2,
+               Form="star"
+                },
+                new CardModel() {Imgbase="assets/cards/2starRed.png",
+                   Color ="red",
+               Number=2,
+               Form="star"
+                },
+                 new CardModel() {Imgbase="assets/cards/2starYellow.png",
+                   Color ="yellow",
+               Number=2,
+               Form="star"
+                },
+                new CardModel() {Imgbase="assets/cards/3circleBlue.png",
+                   Color ="blue",
+               Number=3,
+               Form="circle"
+                },
+                new CardModel() {Imgbase="assets/cards/3circleRed.png",
+                   Color ="red",
+               Number=3,
+               Form="circle"
+                },
+                new CardModel() {Imgbase="assets/cards/3circleYellow.png",
+                   Color ="yellow",
+               Number=3,
+               Form="circle"
+                },
+                new CardModel() {Imgbase="assets/cards/3circleGreen.png",
+                   Color ="green",
+               Number=3,
+               Form="circle"
+                },
+                new CardModel() {Imgbase="assets/cards/3crossBlue.png",
+                   Color ="blue",
+               Number=3,
+               Form="cross"
+               },
+
+                new CardModel() {Imgbase="assets/cards/3crossGreen.png",
+                   Color ="green",
+               Number=3,
+               Form="cross"},
+                new CardModel() {Imgbase="assets/cards/3crossRed.png",
+                   Color ="red",
+               Number=3,
+               Form="cross"
+                },
+                new CardModel() {Imgbase="assets/cards/3squareBlue.png",
+                   Color ="blue",
+               Number=3,
+               Form="square"
+                },
+                new CardModel() {Imgbase="assets/cards/3squareGreen.png",
+                   Color ="green",
+               Number=3,
+               Form="square"
+                },
+                new CardModel() {Imgbase="assets/cards/3squareRed.png",
+                   Color ="red",
+               Number=3,
+               Form="square"
+                },
+                new CardModel() {Imgbase="assets/cards/3squareYellow.png",
+                   Color ="yellow",
+               Number=3,
+               Form="square"
+                },
+                 new CardModel() {Imgbase="assets/cards/3starBlue.png",
+                   Color ="blue",
+               Number=3,
+               Form="star"
+                },
+                new CardModel() {Imgbase="assets/cards/3starGreen.png",
+                   Color ="green",
+               Number=3,
+               Form="star"
+                },
+                new CardModel() {Imgbase="assets/cards/3starRed.png",
+                   Color ="red",
+               Number=3,
+               Form="star"
+                },
+                new CardModel() {Imgbase="assets/cards/3starYellow.png",
+                   Color ="yellow",
+               Number=3,
+               Form="star"
+                },
+                new CardModel() {Imgbase="assets/cards/4circleGreen.png",
+                   Color ="green",
+               Number=4,
+               Form="circle"
+                },
+                new CardModel() {Imgbase="assets/cards/4circleRed.png",
+                   Color ="red",
+               Number=4,
+               Form="circle"
+               },
+                 new CardModel() {Imgbase="assets/cards/4circleYellow.png",
+                   Color ="yellow",
+               Number=4,
+               Form="circle"},
+                new CardModel() {Imgbase="assets/cards/4crossBlue.png",
+                   Color ="blue",
+               Number=4,
+               Form="cross"
+                },
+                new CardModel() {Imgbase="assets/cards/4crossGreen.png",
+                   Color ="green",
+               Number=4,
+               Form="cross"
+                },
+                new CardModel() {Imgbase="assets/cards/4crossRed.png",
+                   Color ="red",
+               Number=4,
+               Form="cross"
+                },
+                new CardModel() {Imgbase="assets/cards/4crossYellow.png",
+                   Color ="yellow",
+               Number=4,
+               Form="cross"
+                },
+                 new CardModel() {Imgbase="assets/cards/4squareBlue.png",
+                   Color ="blue",
+               Number=4,
+               Form="square"
+                },
+                new CardModel() {Imgbase="assets/cards/4squareGreen.png",
+                   Color ="green",
+               Number=4,
+               Form="square"
+                },
+                new CardModel() {Imgbase="assets/cards/4squareRed.png",
+                   Color ="red",
+               Number=4,
+               Form="square"
+                },
+                new CardModel() {Imgbase="assets/cards/4squareYellow.png",
+                   Color ="yellow",
+               Number=4,
+               Form="square"
+                },
+                new CardModel() {Imgbase="assets/cards/4starBlue.png",
+                   Color ="blue",
+               Number=4,
+               Form="star"
+                },
+                new CardModel() {Imgbase="assets/cards/4starGreen.png",
+                   Color ="green",
+               Number=4,
+               Form="star"
+               },
+                new CardModel() {Imgbase="assets/cards/4starRed.png",
+                   Color ="red",
+               Number=4,
+               Form="star"},
+                new CardModel() {Imgbase="assets/cards/4starYellow.png",
+                   Color ="yellow",
+               Number=4,
+               Form="star"
                 }
             };
             return result;
@@ -398,24 +694,6 @@ namespace wcstApp.Services.CardService
 
             //  return images[toSkip];
             return randomizedList;
-        }
-        public async Task<dynamic> GetRandomCard()
-        {
-            WebRequest wr = WebRequest.Create("http://localhost:59308/api/Card/random");
-            WebResponse res = wr.GetResponse();
-            dynamic image;
-            using (StreamReader reader = new StreamReader(res.GetResponseStream()))
-            {
-                string json = reader.ReadToEnd();
-                image = JsonConvert.DeserializeObject(json);
-                //  this.pictureBox1.Load(images["0"].imglink.Value);
-            }
-            dynamic card;
-            if (image.Count > 0)
-            {
-                card = image.Dequeue();
-            }
-            return image;
         }
       
     }
--- wcstApp/wcstApp/wwwroot/angular-wcst/src/app/cards/cards.component.ts
+++ wcstApp/wcstApp/wwwroot/angular-wcst/src/app/cards/cards.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from "@angular/core";
-import {
-  DomSanitizer,
-  SafeResourceUrl
-} from "../../../node_modules/@angular/platform-browser";
+import { SafeResourceUrl } from "../../../node_modules/@angular/platform-browser";
 import { CardsModel } from "../service/CardsService/cards.model";
 import { CardsService } from "../service/CardsService/cards.service";
 import { RuleService } from "../service/RuleService/rule.service";
 import { RuleModel } from "../service/RuleService/rule.model";
-import { timer } from "../../../node_modules/rxjs/internal/observable/timer";
+import { timer } from "rxjs";
 import { timeInterval, pluck, take } from "rxjs/operators";
+
 @Component({
   selector: "app-cards",
   templateUrl: "./cards.component.html",
@@ -17,31 +15,48 @@ import { timeInterval, pluck, take } from "rxjs/operators";
 export class CardsComponent implements OnInit {
   startingCards: Array<CardsModel> = [];
   card: CardsModel;
-  cards: Array<CardsModel>=[];
+  deck: Array<CardsModel> = [];
 
-  previousCard: CardsModel;
-  imagePaths: Array<SafeResourceUrl> = [];
-  randomImagePath: SafeResourceUrl;
-  isRuleChecked: boolean;
-  decodedCards: Array<string>;
-  rule: RuleModel;
+  //   previousCard: CardsModel;
+  //   imagePaths: Array<SafeResourceUrl> = [];
+  //  randomImagePath: SafeResourceUrl;
+  //  previousRule: RuleModel = { name };
+  //  decodedCards: Array<string>;
+  //  rule: RuleModel;
+  
+  previousErrorRule: string;
   ruleCounter: number;
   result: string;
+  isTestEnded: boolean = false;
   isStarted: boolean;
-  previousRule: RuleModel = { name };
-  mobile:boolean=false;
-  ruleName:string;
+  isRuleChecked: boolean;
+  mobile: boolean = false;
+  ruleName: string;
   //scoring
+  trial: number;
   errors: number;
-  responses:number;
-  completedCategories:number;
+  responses: number;
+  completedCategories: number;
+  time: string;
+  nonPerservativeErrors: number;
+  perservativeErrors: number;
+  perservativeResponses: number;
+  percentageOfResponses: number;
+  percentageOfPerservativeErrors: number;
+  failureToSet: number;
+  trialsToFirstCategory: number;
+  series: number;
+
+  conceptualLevelResonse: number;
+  learningToLearn: number;
   //stopwatch:
+
   x: number;
   intervalId;
   hour = 0;
   minute = 0;
   second = 0;
-  millisecond = 0;
+  millisecond = 0; // czy potrzebne??
   pause = false;
 
   constructor(
@@ -50,28 +65,34 @@ export class CardsComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-      if(window.screen.width<1024){
-          this.mobile=true;
-      }
+    if (window.screen.width < 1024) {
+      this.mobile = true;
+    }
+    this.trial = 1;
     this.errors = 0;
     this.ruleCounter = 0;
-    this.responses=0;
-    this.completedCategories=0;
-    this.ruleName="color";
+    this.responses = 0;
+    this.completedCategories = 0;
+    this.nonPerservativeErrors = 0;
+    this.perservativeErrors = 0;
+    this.perservativeResponses = 0;
+    this.trialsToFirstCategory = 0;
+    this.series = 0;
+    this.conceptualLevelResonse = 0;
+    this.failureToSet = 0;
+    this.previousErrorRule = "";
+    this.ruleName = "color";
     this.getDeck();
   }
-
   getStartingCards() {
     this.cardsService.getStartingCards().subscribe(r => {
       this.startingCards = r;
       this.isStarted = true;
       // console.log(this.startingCards)
       //  this.decode()
-      this.onStartStopwatch()
-      
+      this.onStartStopwatch();
     });
   }
-
   // decode(){
   //     this.imagePaths=[]
   //     this.startingCards.forEach(card => {
@@ -80,14 +101,22 @@ export class CardsComponent implements OnInit {
   // }
   getDeck() {
     this.cardsService.getDeck().subscribe(r => {
-// this.card=r;     
-        this.cards = r;
-     // this.isCardRepeated();
+      // this.card=r;
+      this.deck = r;
+      // this.isCardRepeated();
       //this.randomImagePath=this.sanitizer.bypassSecurityTrustUrl('data:image/png;base64,' + r.imgbase);
     });
   }
-  getRandomCard(){
-    this.card= this.cards.pop();
+  getRandomCard() {
+    if (this.deck.pop()) this.card = this.deck.pop();
+    else {
+      this.isTestEnded = true;
+      this.pause = true;
+      clearInterval(this.intervalId);
+      this.time = this.hour + "h " + this.minute + "m " + this.second + "s";
+      console.log("KONIEC!!!!!!");
+      this.makeScoring();
+    }
   }
   checkCard(element) {
     console.log(element);
@@ -95,61 +124,166 @@ export class CardsComponent implements OnInit {
     switch (this.ruleName) {
       case "color":
         if (element.color == this.card.color) {
-          this.ruleCounter = this.ruleCounter + 1;
-          this.responses=this.responses+1;
+          this.ruleCounter++;
+          this.responses++;
+          this.trial++;
           console.log("DOBRZE! ruleCounter:", this.ruleCounter);
           this.result = "Dobrze!";
+          this.calculateTrialsToFirstCategory();
+          this.calculateConceptualLevelResponse();
         } else {
           this.errors++;
-          this.ruleCounter=0;
+          this.calculateFailureToSet();
+          this.ruleCounter = 0;
+          this.series = 0;
+          this.trial++;
           console.log("ZLE! mistakeCounter:", this.errors);
           this.result = "Źle!";
+          this.calculateTrialsToFirstCategory();
+          if (
+            this.completedCategories > 0 &&
+            element.number == this.card.number
+          ) {
+            this.perservativeResponses++;
+            console.log("pr: ", this.perservativeResponses);
+          }
+          if (element.number == this.card.number) {
+            this.previousErrorRule = "number";
+          } else {
+            this.previousErrorRule = "form";
+          }
+          this.checkPerseveration(element);
         }
         break;
       case "form":
         if (element.form == this.card.form) {
-          this.ruleCounter = this.ruleCounter + 1;
-          this.responses=this.responses+1;
+          this.ruleCounter++;
+          this.responses++;
+          this.trial++;
           console.log("DOBRZE! ruleCounter:", this.ruleCounter);
           this.result = "Dobrze!";
+          this.calculateTrialsToFirstCategory();
+          this.calculateConceptualLevelResponse();
         } else {
           this.errors++;
-          this.ruleCounter=0;
+          this.calculateFailureToSet();
+          this.ruleCounter = 0;
+          this.series = 0;
+          this.trial++;
           console.log("ZLE! mistakeCounter:", this.errors);
           this.result = "Źle!";
+          this.calculateTrialsToFirstCategory();
+          if (
+            this.completedCategories > 0 &&
+            element.color == this.card.color
+          ) {
+            this.perservativeResponses++;
+            console.log("pr: ", this.perservativeResponses);
+          }
+          if (element.number == this.card.number) {
+            this.previousErrorRule = "number";
+          } else {
+            this.previousErrorRule = "color";
+          }
         }
         break;
       case "number":
         if (element.number == this.card.number) {
-          this.ruleCounter = this.ruleCounter + 1;
-          this.responses=this.responses+1;
+          this.ruleCounter++;
+          this.responses++;
+          this.trial++;
           console.log("DOBRZE! ruleCounter:", this.ruleCounter);
+          this.calculateTrialsToFirstCategory();
+          this.calculateConceptualLevelResponse();
           this.result = "Dobrze!";
         } else {
           this.errors++;
-          this.ruleCounter=0;
+          this.calculateFailureToSet();
+          this.ruleCounter = 0;
+          this.series = 0;
+          this.trial++;
           console.log("ZLE! mistakeCounter:", this.errors);
           this.result = "Źle!";
+          this.calculateTrialsToFirstCategory();
+          if (this.completedCategories > 0 && element.form == this.card.form) {
+            this.perservativeResponses++;
+            console.log("pr: ", this.perservativeResponses);
+          }
+          if (element.color == this.card.color) {
+            this.previousErrorRule = "color";
+          } else {
+            this.previousErrorRule = "form";
+          }
         }
         break;
     }
     this.getRandomCard();
     if (this.ruleCounter == 10) {
       this.ruleCounter = 0;
-      this.completedCategories=this.completedCategories+1;
+      this.completedCategories++;
+      if (this.completedCategories == 6) {
+        this.isTestEnded = true;
+        this.pause = true;
+        clearInterval(this.intervalId);
+        this.time = this.hour + "h " + this.minute + "m " + this.second + "s";
+        this.makeScoring();
+      }
       console.log("ZMIANA REGUŁY! ruleCounter:", this.ruleCounter);
       this.getRandomRule();
     }
   }
-  isCardRepeated() {
-    if (JSON.stringify(this.card) === JSON.stringify(this.previousCard)) {
-      console.log("KARTY SIE POWTORZYLY!!!!");
-
-      this.getDeck();
-    } else {
-      this.previousCard = this.card;
+  calculateTrialsToFirstCategory() {
+    if (this.completedCategories < 1) {
+      this.trialsToFirstCategory++;
+    }
+    if (this.isTestEnded && this.completedCategories == 0) {
+      this.trialsToFirstCategory = 129;
     }
   }
+  calculateFailureToSet() {
+    if (this.ruleCounter > 4 && this.ruleCounter < 10) {
+      this.failureToSet++;
+    }
+  }
+  calculateConceptualLevelResponse() {
+    this.series++;
+    if (this.series == 3) {
+      this.conceptualLevelResonse = this.conceptualLevelResonse + 3;
+    } else if (this.series > 3) {
+      this.conceptualLevelResonse++;
+    }
+    //ToDo
+  }
+  checkPerseveration(element) {
+    if (element.color == this.card.color && this.previousErrorRule == "color") {
+      this.perservativeErrors = this.perservativeErrors + 1;
+    } else if (
+      element.number == this.card.number &&
+      this.previousErrorRule == "number"
+    ) {
+      this.perservativeErrors = this.perservativeErrors + 1;
+    } else if (
+      element.form == this.card.form &&
+      this.previousErrorRule == "form"
+    ) {
+      this.perservativeErrors = this.perservativeErrors + 1;
+    } else {
+      this.nonPerservativeErrors = this.nonPerservativeErrors + 1;
+    }
+  }
+  makeScoring() {
+    this.percentageOfPerservativeErrors = this.perservativeErrors / this.errors;
+    this.percentageOfResponses = this.responses / this.trial;
+  }
+  //   isCardRepeated() {
+  //     if (JSON.stringify(this.card) === JSON.stringify(this.previousCard)) {
+  //       console.log("KARTY SIE POWTORZYLY!!!!");
+
+  //       this.getDeck();
+  //     } else {
+  //       this.previousCard = this.card;
+  //     }
+  //   }
   getRandomRule() {
     // this.ruleService.getRandomRule().subscribe(r => {
     //   this.rule = r;
@@ -157,27 +291,27 @@ export class CardsComponent implements OnInit {
     //   this.isRuleChecked = true;
     // });
     switch (this.ruleName) {
-        case "color":
-        this.ruleName="form";
+      case "color":
+        this.ruleName = "form";
         break;
-        case "form":
-        this.ruleName="number";
+      case "form":
+        this.ruleName = "number";
         break;
-        case "number":
-        this.ruleName="color";
+      case "number":
+        this.ruleName = "color";
         break;
     }
   }
-  isRuleRepeated() {
-    if (
-      JSON.stringify(this.rule).toLowerCase() ===
-      JSON.stringify(this.previousRule).toLowerCase()
-    ) {
-      this.getRandomRule();
-    } else {
-      this.previousRule = this.rule;
-    }
-  }
+  //   isRuleRepeated() {
+  //     if (
+  //       JSON.stringify(this.rule).toLowerCase() ===
+  //       JSON.stringify(this.previousRule).toLowerCase()
+  //     ) {
+  //       this.getRandomRule();
+  //     } else {
+  //       this.previousRule = this.rule;
+  //     }
+  //   }
   //stopwatch:
   onStartStopwatch() {
     this.x = 10;
@@ -187,17 +321,14 @@ export class CardsComponent implements OnInit {
   }
   updateTime() {
     this.millisecond += this.x;
-
     if (this.millisecond > 90) {
       this.millisecond = 0;
       this.second++;
     }
-
     if (this.second > 59) {
       this.second = 0;
       this.minute++;
     }
-
     if (this.minute > 59) {
       this.minute = 0;
       this.hour++;
